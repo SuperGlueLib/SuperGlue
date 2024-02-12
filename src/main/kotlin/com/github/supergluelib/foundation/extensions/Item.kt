ca5cdb400800e4f1213a15064b06305756904fda
MLGPenguin <baww123@gmail.com>
2024-02-12 17:21:17 +1000

    + ItemStack.getPersistent for easy PDC access

--- src/main/kotlin/com/github/supergluelib/foundation/extensions/Item.kt
+++ src/main/kotlin/com/github/supergluelib/foundation/extensions/Item.kt
@@ -1,8 +1,10 @@
 package com.github.supergluelib.foundation.extensions
 
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.persistence.PersistentDataType
 
 /** Decrement the amount of the itemstack by 1 */
 fun ItemStack.removeOne() { amount -= 1 }
@@ -14,6 +16,9 @@ fun ItemStack.isBasic() = this.isSimilar(ItemStack(type))
 /** @return true if the localized name of this item exists and is equal to [loc] */
 fun ItemStack.locnameIs(loc: String): Boolean = itemMeta?.localizedName == loc
 
+/** @return The value gotten from the persistent data container of this item using the given key and type */
+private fun <T> ItemStack.getPersistent(key: NamespacedKey, type: PersistentDataType<T, T>) = itemMeta?.persistentDataContainer?.get(key, type)
+
 /** @return true if the localized name of this item exists and is equal to [loc] */
 fun ItemMeta.locnameIs(loc: String): Boolean = localizedName == loc
 
@@ -21,3 +26,4 @@ fun ItemMeta.locnameIs(loc: String): Boolean = localizedName == loc
 fun Material.formattedName() = toString()
     .split("_")
     .joinToString(" ") { it.lowercase().replaceFirstChar(Char::uppercase) }
+
