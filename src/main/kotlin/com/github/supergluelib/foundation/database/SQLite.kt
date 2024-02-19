ced71f9bd198bc60b6dd67446e0a5bed90e55015
MLGPenguin <baww123@gmail.com>
2024-02-19 16:57:15 +1000

    + Create database file if it does not exist

--- src/main/kotlin/com/github/supergluelib/foundation/database/SQLite.kt
+++ src/main/kotlin/com/github/supergluelib/foundation/database/SQLite.kt
@@ -1,12 +1,14 @@
 package com.github.supergluelib.foundation.database
 
+import java.io.File
 import java.sql.DriverManager
 
 /**
  * Handles an instance of a SQLite Database, Make sure to close it before the server shuts down.
  * Automatically intialises a connection when the class is created
  */
-class SQLiteDatabase(val filePath: String) {
+class SQLiteDatabase(val file: File) {
+    private val filePath = file.apply { parentFile.mkdirs() }.path
     val connection = DriverManager.getConnection("jdbc:sqlite:$filePath")
     // TODO: return auto generated keys with prepareStatement(string, int)
 
