27e3f2c6b1ac84bd9c5275a73e1a87cb01d37a31
MLGPenguin <baww123@gmail.com>
2024-02-23 00:08:28 +1000

    + BlockPos .add & .subtract

--- src/main/kotlin/com/github/supergluelib/foundation/misc/BlockPos.kt
+++ src/main/kotlin/com/github/supergluelib/foundation/misc/BlockPos.kt
@@ -14,4 +14,10 @@ data class BlockPos(val x: Int, val y: Int, val z: Int) {
     fun getBlock(world: World) = world.getBlockAt(x, y, z)
 
     fun isLoaded(world: World) = world.isChunkLoaded(x shr 4, z shr 4)
+
+    /** @return a new BlockPos containing the addition of each coordinate of the original two BlockPos's */
+    fun add(other: BlockPos) = BlockPos(other.x + x, other.y + y, other.z + z)
+
+    /** @return a new BlockPos which has the coordinates of the [other] BlockPos subtracted from this BlockPos */
+    fun subtract(other: BlockPos) = BlockPos(x - other.x, y - other.y, z - other.z)
 }
