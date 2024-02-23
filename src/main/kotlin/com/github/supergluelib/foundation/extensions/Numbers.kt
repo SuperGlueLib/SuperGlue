9c86ff5a8ae1492d0aa1910026a044247ac4adbc
MLGPenguin <baww123@gmail.com>
2024-02-23 20:50:53 +1000

    + Long.format()

--- src/main/kotlin/com/github/supergluelib/foundation/extensions/Numbers.kt
+++ src/main/kotlin/com/github/supergluelib/foundation/extensions/Numbers.kt
@@ -9,4 +9,5 @@ fun Double.round(precision: Int): Double = 10.pow(precision).let { if (precision
 fun Float.round(precision: Int): Float = toDouble().round(precision).toFloat()
 fun Double.format(decimals: Int = 2) = DecimalFormat.getInstance().apply { maximumFractionDigits = decimals }.apply { isGroupingUsed = true }.apply { minimumFractionDigits = 2 }.format(this)
 fun Int.format() = DecimalFormat.getIntegerInstance().apply { isGroupingUsed = true }.format(this)
+fun Long.format() = DecimalFormat.getIntegerInstance().apply { isGroupingUsed = true }.format(this)
 infix fun Int.pow(other: Int): Int = (1..other).fold(1) { acc, _ -> acc * this }
