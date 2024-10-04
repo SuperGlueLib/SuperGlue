5bcc1d566638870d960352a1fff21a85007e185a
MLGPenguin <baww123@gmail.com>
2024-10-05 01:33:18 +1000

    1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 val groupName = "com.github.supergluelib"
 val artifactName = "SuperGlue"
-val libraryVersion = "1.1.1"
+val libraryVersion = "1.1.2"
 group = groupName
 version = libraryVersion
 
