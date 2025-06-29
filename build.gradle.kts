a1a43d265cb401bcdbff1727b2f91cbea20aefd4
MLGPenguin <baww123@gmail.com>
2025-06-29 22:35:18 +1000

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 val groupName = "com.github.supergluelib"
 val artifactName = "SuperGlue"
-val libraryVersion = "1.2.5"
+val libraryVersion = "1.2.6"
 group = groupName
 version = libraryVersion
 
